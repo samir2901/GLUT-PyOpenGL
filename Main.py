8fa3541cbba8e8823910fcdcb07d25365e64a0a5
samir2901 <thunder.cse2018@gmail.com>
2021-01-30 13:55:36 +0530

    Update

--- Main.py
+++ Main.py
@@ -2,7 +2,6 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import sys
-from Clock import *
 
 def init(width,height):
     glClearColor(0.0,0.0,0.0,1.0)        
@@ -13,7 +12,7 @@ def init(width,height):
 def display():
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
     
-    drawClock()    
+    
     
     glutSwapBuffers()     
 
@@ -27,5 +26,4 @@ glutInitWindowSize(width,height)
 wind = glutCreateWindow("OpenGL")
 init(width,height)
 glutDisplayFunc(display)
-glutTimerFunc(1000,clock,0)
 glutMainLoop()
